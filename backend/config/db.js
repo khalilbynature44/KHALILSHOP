c0807e3bdc90621550eb3c874d90e2e6164d2861
khalil baklouti <khalilbaklouti42@gmail.com>
2022-04-27 23:34:07 +0100

    add adress  database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect( "mongodb://localhost:27017/khalil", {
+    const conn = await mongoose.connect( "mongodb+srv://test:865XF430g8MADDdk@cluster0.dl9mo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
